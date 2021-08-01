19d5c972329de4317eb91e4309920dec52bd83c4
saad143412 <88199414+saad143412@users.noreply.github.com>
2021-08-01 18:22:28 +0500

    Add files via upload

---
+++ LAB1 Area and Circumeference.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	/*Programmer Name:Muhammad Saad
+	  Reg.No:20MDELE163             */
+  	 
+	printf("Programmer Name:Muhammad Saad\nReg.No:20MDELE163\n\n");
+	
+	int r;
+	float pi=3.14,Area,Circumference;
+	printf("please Enter a radius: ");
+	scanf("%d",&r);
+	Area = pi*r*r;
+	Circumference = 2*pi*r;
+	printf("The Required Area of a Circle is:%g",Area);
+	printf("\nThe Required Circumference of a Circle is:%g",Circumference);
+	
+	
+}
+
+
---
+++ PF LAB 01 TASK1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(){
+	int length = 8;
+	int width  = 8;
+	printf("The Area of rectangle is: %d", length*width);
+	
+}
